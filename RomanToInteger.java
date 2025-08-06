46d1a5066514d5896584700e4a3a9e868d0fa581
G.Lokeshbabu <lokeshbabugorrepati@gmail.com>
2025-08-06 21:14:23 +0530

    Add files via upload

---
+++ RomanToInteger.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int romanToDecimal(String s) {
+        // Map Roman numerals to their integer values
+        java.util.HashMap<Character, Integer> map = new java.util.HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int total = 0;
+        int prev = 0;
+
+        // Traverse from right to left
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int curr = map.get(s.charAt(i));
+
+            if (curr < prev) {
+                total -= curr;
+            } else {
+                total += curr;
+            }
+
+            prev = curr;
+        }
+
+        return total;
+    }
+}
